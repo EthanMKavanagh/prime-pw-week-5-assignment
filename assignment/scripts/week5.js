dcfc880d1abb8bf6d87f4d8ecfd555c9ee3deb12
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-13 09:33:22 -0500

    added css and html text

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,1 +1,17 @@
 console.log('js');
+
+
+//addToCollection function
+//-----------------------
+
+let collection = [];
+
+function addToCollection( title, artist, year ){
+  console.log( 'in addToCollection' );
+  if( collection ){
+    collection.push( title, artist, year );
+    console.log( 'Awesome track dude!', collection );
+  } // end if
+} // end addToCollection
+
+addToCollection( 'Be Something', 'Polo G', 2020 )
